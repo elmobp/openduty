d5e1c82baced13f76b74d60826c87e165f180faa
Michael Baker <michael.baker@bulletproof.net>
2019-08-22 13:41:35 +0800

    NEw code

--- notification/notifier/twilio_sms.py
+++ notification/notifier/twilio_sms.py
@@ -1,6 +1,6 @@
 __author__ = 'deathowl'
 
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 import twilio
 import sys
 
@@ -12,9 +12,9 @@ class TwilioSmsNotifier:
     def notify(self, notification):
         max_length = 160
         message = (notification.message[:max_length-2] + '..') if len(notification.message) > max_length else notification.message
-        client = TwilioRestClient(self.__config['SID'], self.__config['token'])
+        client = Client(self.__config['SID'], self.__config['token'])
         try:
-            client.sms.messages.create(body=message,
+            client.messages.create(body=message,
                 to=notification.user_to_notify.profile.phone_number,
                 from_=self.__config['sms_number'])
             print 'successfully sent the sms'
